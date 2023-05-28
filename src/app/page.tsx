6afa2f73dee3bf95b9f0e6b15f99242edf467b0e
Filipe Oliveira <hi@fmoliveira.dev>
2023-05-28 19:05:40 -0400

    Customize page styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AgendaDisplay from "@/components/AgendaDisplay";
 
 export default function Home() {
   return (
-    <main>
+    <main className="container max-w-screen-lg mx-auto px-4 py-4">
       <AgendaDisplay />
     </main>
   );
--- src/components/AgendaDisplay.tsx
+++ src/components/AgendaDisplay.tsx
@@ -5,47 +5,59 @@ const agenda = getSummitAgenda();
 export default function AgendaDisplay() {
   return (
     <div>
-      <h1>AWS Summit Toronto 2023</h1>
+      <h1 className="text-3xl font-bold py-3">AWS Summit Toronto 2023</h1>
       <p>
         <a
           href="https://aws.amazon.com/events/summits/toronto/"
           target="_blank"
           rel="noreferrer noopener"
+          className="text-cyan-200 hover:text-indigo-400 hover:underline"
         >
           https://aws.amazon.com/events/summits/toronto/
         </a>
       </p>
       {agenda.map((daySchedule) => (
-        <div key={daySchedule.day}>
-          <h2>{daySchedule.day}</h2>
-          <div>
-            {daySchedule.interval.map((interval) => (
-              <div key={interval.duration}>
-                <h3>{interval.duration}</h3>
-                <ul>
-                  {interval.sessions.map((session) => (
-                    <li key={session.headline}>
-                      <strong>
-                        {session.category} &lt;&gt; {session.headline}
-                      </strong>
-                      <br />
-                      <span>
-                        <strong>Hosts:</strong> {session.subHeadline}
-                      </span>
-                      <br />
-                      <span>
-                        <strong>Expertise:</strong> {session.expertise}
-                      </span>
-                      <br />
-                      <br />
-                      <div>{session.description}</div>
-                      <br />
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div>
+        <div key={daySchedule.day} className="my-4">
+          <h2 className="text-2xl font-bold py-2">{daySchedule.day}</h2>
+          {daySchedule.interval.map((interval) => (
+            <div key={interval.duration} className="mb-8">
+              <h3 className="text-xl font-bold py-1 text-amber-400">
+                {interval.duration}
+              </h3>
+              <ul>
+                {interval.sessions.map((session) => (
+                  <li
+                    key={session.headline}
+                    className="my-4 ml-4 pl-4 py-1 flex flex-col gap-2 border-l-2 border-indigo-300"
+                  >
+                    <p className="font-bold text-cyan-400">
+                      {session.category} &lt;&gt; {session.headline}
+                    </p>
+                    {!!session.subHeadline && (
+                      <p>
+                        <span className="text-slate-200 font-semibold">
+                          Hosts:
+                        </span>{" "}
+                        {session.subHeadline}
+                      </p>
+                    )}
+                    {!!session.expertise && (
+                      <p>
+                        <span className="text-slate-200 font-semibold">
+                          Expertise:
+                        </span>{" "}
+                        {session.expertise}
+                        <br />
+                      </p>
+                    )}
+                    <p className="text-slate-300 leading-7">
+                      {session.description}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       ))}
     </div>
